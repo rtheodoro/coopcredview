478e6fd94f2322482bd77516e5b2517ed9a23e80
rtheodoro <rtheodoro@usp.br>
2023-08-05 00:27:13 -0300

    grafico de genero adicionado

--- R/mod_estrut_gov.R
+++ R/mod_estrut_gov.R
@@ -10,10 +10,22 @@
 mod_estrut_gov_ui <- function(id){
   ns <- NS(id)
 
-  bs4Dash::box(
-        title = "Estrutura de Governança das Cooperativas de Crédito em 07/2022",
-     reactable::reactableOutput(ns("tabela_governanca"))
+  tagList(
+     bs4Dash::box(title = "Estrutura de Governança das Cooperativas de Crédito em 07/2022",
+                  reactable::reactableOutput(ns(
+                     "tabela_governanca"
+                  ))),
+     bs4Dash::box(
+        title = tippy::tippy("Porcentagem de Homens e Mulheres por Cargos", "em 07/2022"),
+        shiny::selectInput(
+           ns("cooperativa_select"),
+           "Escolha uma Cooperativa:",
+           choices = c("Todas", read.csv(app_sys("data/202307_CoopCred_BCB_estrutura_governanca.csv")) |> dplyr::select(nomec) |> unique())
+        ),
+        plotly::plotlyOutput(ns("genero_por_cargo"))
+     )
   )
+
 }
 
 #' estrut_gov Server Functions
@@ -42,6 +54,28 @@ mod_estrut_gov_server <- function(id){
           bordered = TRUE,          # Borda nas células
           pagination = TRUE         # Paginação
        )
+    })
+
+    output$genero_por_cargo <- plotly::renderPlotly({
+       cooperativa_selecionada <- input$cooperativa_select
+
+       if (cooperativa_selecionada == "Todas") {
+          dados_filtrados <- estrut_gov
+       } else {
+          dados_filtrados <- subset(estrut_gov, nomec == cooperativa_selecionada)
+       }
+
+       genero_counts <- table(dados_filtrados$cargo, dados_filtrados$genero)
+       genero_data <- data.frame(
+          Cargo = rep(rownames(genero_counts), ncol(genero_counts)),
+          Genero = rep(colnames(genero_counts), each = nrow(genero_counts)),
+          Count = as.vector(genero_counts)
+       )
+
+       genero_data$Genero <- ifelse(genero_data$Genero == "H", "Masculino", "Feminino")
+
+       plotly::plot_ly(data = genero_data, x = ~Cargo, y = ~Count, color = ~Genero,
+                       type = "bar", marker = list(line = list(color = "white")))
     })
 
   })
--- data-raw/rascunho.R
+++ data-raw/rascunho.R
@@ -1,33 +1,10 @@
-
-balanco <- data.table::fread(app_sys("data/balanco_coop_cred_2010a2022_4010.csv"))
-dicionario <- read.csv(app_sys("data/dicionario.CSV")) |> janitor::clean_names()
-
-dicionario <- dicionario |>
-   dplyr::mutate(conta = as.numeric(conta)) |>
-   dplyr::filter(!is.na(conta)) |>
-   unique()
-
-
+# Renomear as colunas automaticamente
 colnames(balanco) <- stringr::str_remove(colnames(balanco), "^X")
 
 nomes_correspondentes <- dicionario$nome_conta[match(names(balanco), dicionario$conta)]
 
-# Renomear as colunas automaticamente
+
 colnames(balanco) <- ifelse(!is.na(nomes_correspondentes),
                             paste(names(balanco), nomes_correspondentes, sep = "_"),
                             names(balanco))
 
-
-
-cnpj_2022 <- balanco |> dplyr::filter(ano == "202212") |> dplyr::select(cnpj)
-balanco <- balanco |> dplyr::filter(cnpj %in% cnpj_2022$cnpj)
-
-write.csv(balanco, "inst/data/balanco_coop_cred_2010a2022_4010.csv", row.names = FALSE)
-
-
-
-balanco |>
-   dplyr::filter(cnpj == 1060307) |>
-   dplyr::mutate(ano = as.Date(sprintf("%s-%s-01", substr(ano, 1, 4), substr(ano, 5, 6)))) |>
-   echarts4r::e_chart(x = ano) |>
-   echarts4r::e_line(serie = X11000006_DISPONIBILIDADES)
