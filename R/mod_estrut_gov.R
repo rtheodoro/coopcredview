0abad32cad53c07604028370f7d503fc72868eb4
rtheodoro <rtheodoro@usp.br>
2023-08-05 01:21:30 -0300

    grafico de genero pronto

--- R/mod_estrut_gov.R
+++ R/mod_estrut_gov.R
@@ -16,11 +16,18 @@ mod_estrut_gov_ui <- function(id){
                      "tabela_governanca"
                   ))),
      bs4Dash::box(
-        title = tippy::tippy("Porcentagem de Homens e Mulheres por Cargos", "em 07/2022"),
+        title = tippy::tippy("Contagem de Gênero em Cooperativas de Crédito em 07/2022",
+                             "As informações foram geradas a partir do primeiro nome, podem ter um grau de imprecisão"),
         shiny::selectInput(
            ns("cooperativa_select"),
            "Escolha uma Cooperativa:",
            choices = c("Todas", read.csv(app_sys("data/202307_CoopCred_BCB_estrutura_governanca.csv")) |> dplyr::select(nomec) |> unique())
+        ),
+        shiny::radioButtons(
+           ns("info_type"),
+           label = "Ver informações por:",
+           choices = c("Órgão", "Cargo"),
+           selected = "Órgão"
         ),
         plotly::plotlyOutput(ns("genero_por_cargo"))
      )
@@ -56,29 +63,66 @@ mod_estrut_gov_server <- function(id){
        )
     })
 
-    output$genero_por_cargo <- plotly::renderPlotly({
-       cooperativa_selecionada <- input$cooperativa_select
-
-       if (cooperativa_selecionada == "Todas") {
-          dados_filtrados <- estrut_gov
+    filtered_data <- reactive({
+       if (input$info_type == "Cargo") {
+          data <- estrut_gov
+          x_axis <- "cargo"
        } else {
-          dados_filtrados <- subset(estrut_gov, nomec == cooperativa_selecionada)
+          data <- subset(estrut_gov, !is.na(orgao))
+          x_axis <- "orgao"
        }
 
-       genero_counts <- table(dados_filtrados$cargo, dados_filtrados$genero)
+       if (input$cooperativa_select != "Todas") {
+          data <- subset(data, nomec == input$cooperativa_select)
+       }
+
+       list(data = data, x_axis = x_axis)
+    })
+
+    output$genero_por_cargo <- plotly::renderPlotly({
+       data <- filtered_data()$data
+       data$genero <- as.character(data$genero)
+       genero_counts <- table(data[[filtered_data()$x_axis]], data$genero)
        genero_data <- data.frame(
-          Cargo = rep(rownames(genero_counts), ncol(genero_counts)),
+          CargoOrgao = rep(rownames(genero_counts), ncol(genero_counts)),
           Genero = rep(colnames(genero_counts), each = nrow(genero_counts)),
-          Count = as.vector(genero_counts)
+          Contagem = as.vector(genero_counts)
        )
 
        genero_data$Genero <- ifelse(genero_data$Genero == "H", "Masculino", "Feminino")
 
-       plotly::plot_ly(data = genero_data, x = ~Cargo, y = ~Count, color = ~Genero,
-                       type = "bar", marker = list(line = list(color = "white")))
-    })
+       x_axis_label <- if (input$info_type == "Cargo") {
+          "Cargo"
+       } else {
+          "Órgão"
+       }
 
+       plotly::plot_ly(
+          data = genero_data,
+          x = ~CargoOrgao,
+          y = ~Contagem,
+          color = ~Genero,
+          colors = c(Masculino = "#C9E8F5", Feminino = "#C9F5D1"),
+          type = "bar",
+          marker = list(line = list(color = "white"))
+       )  |>
+          plotly::layout(
+             xaxis = list(title = x_axis_label),
+             showlegend = TRUE,
+             legend = list(
+                x = 1,
+                y = 1,
+                traceorder = "normal",
+                font = list(family = "sans-serif", size = 12, color = "black"),
+                bgcolor = "white",
+                bordercolor = "#E2E2E2",
+                borderwidth = 2
+             )
+          )
+    })
   })
+
+
 }
 
 ## To be copied in the UI
